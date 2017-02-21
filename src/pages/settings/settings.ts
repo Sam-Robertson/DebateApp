0e6e63551c4edc689a0baa43d52d74bbc7b88b69
Sam Robertson <robertsonnew@gmail.com>
2017-02-21 07:58:03 -0700

    just pulled

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -12,14 +12,19 @@ export class SettingsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public modalCtrl: ModalController) { }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
-  }
   showAlert() {
     let alert = this.alertCtrl.create({
       title: 'Passwords Don\'t match!',
       subTitle: 'Check your Passwords and try again!',
       buttons: ['OK']
+    });
+    alert.present();
+  }
+  showColorAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'You Been Pranked!',
+      subTitle: 'Here at DebateApp we believe in color blind & non-color blind people living in harmony. This is why we make all our Websites and Apps colorblind friendly to begin with',
+      buttons: ['OK', 'I hate you']
     });
     alert.present();
   }
