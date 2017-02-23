ee468efdd6dcf46d455265738d912b445746b3ff
ThargelionZ <measles10@gmail.com>
2017-02-23 09:14:14 -0700

    Added json storage file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { SettingsPage } from "../settings/settings";
 import { LoadingController } from "ionic-angular/index";
 import { DebatePage } from "../debate/debate";
+import { AngularFire, FirebaseListObservable } from "angularfire2";
 
 /*
   Generated class for the Home page.
@@ -16,7 +17,10 @@ import { DebatePage } from "../debate/debate";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingController: LoadingController) {}
+  topics: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingController: LoadingController, public af: AngularFire) {
+    this.topics = af.database.list("/topics");
+  }
 
 
   goToSettings(): void {
@@ -26,158 +30,6 @@ export class HomePage {
   openTopic($event, topic): void {
     this.navCtrl.push(DebatePage, topic);
   }
-
-  topics: Object[] = [
-    {
-      name: "Gun Control",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Drinking Age - Lower it?",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Abortion",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Under God in the Pledge",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Illegal Immigration",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Death Penalty",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "School Uniforms",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Recreational Marijuana",
-      type: "Politics",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Batman vs. Superman",
-      type: "Entertainment",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Video Games and Violence",
-      type: "Entertainment",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Social Networking - Good or Bad?",
-      type: "Entertainment",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Animal Testing",
-      type: "Science and Technology",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Cell Phones - Are They Safe?",
-      type: "Science and Technology",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Alternate Energy vs Fossil Fuels",
-      type: "Science and Technology",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Gay Marriage",
-      type: "Sex and Gender",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-    {
-      name: "Prostitution - Legalize it?",
-      type: "Sex and Gender",
-      totalClicks: 0,
-      proClicks: 0,
-      conClicks: 0,
-      proPercent: "0%",
-      conPercent: "0%"
-    },
-  ];
 }
 
-//http://www.createdebate.com/browse/debaterss/
-
-// topic: "Gun Control",
-//   topic: "Abortion",
-//   topic: "Illegal Immigration",
-//   topic: "Death Penalty", topic: "School Uniforms"];
+//https://debate-app-72076.firebaseio.com/topics/#/property
