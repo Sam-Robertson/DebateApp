a3450ab2891c180a62310642c01c24a0017f9239
ThargelionZ <measles10@gmail.com>
2017-02-24 10:26:36 -0700

    Fixed small bug with imports

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { SettingsPage } from "../settings/settings";
 import { LoadingController } from "ionic-angular/index";
 import { DebatePage } from "../debate/debate";
+import {AngularFire, FirebaseObjectObservable} from "angularfire2";
+
 
 
 /*
