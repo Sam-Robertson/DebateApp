ab069dd02c95500bd6dd18fbbd6eff2007f8e65a
ThargelionZ <measles10@gmail.com>
2017-03-02 10:00:38 -0700

    User can change their vote and implemented toasters

--- src/pages/debate/debate.ts
+++ src/pages/debate/debate.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import {NavController, NavParams, LoadingController} from 'ionic-angular';
+import {NavController, NavParams, LoadingController, ToastController} from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from "angularfire2";
 import { AuthService } from "../../providers/auth-service";
 import {FirebaseService} from "../../providers/firebase-service";
-import {Toast} from "ionic-native";
 
 
 /*
@@ -25,7 +24,7 @@ export class DebatePage {
   users: any[];
   firebaseUsers: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire, private _auth: AuthService, public loadingCtrl: LoadingController, public fb: FirebaseService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire, private _auth: AuthService, public loadingCtrl: LoadingController, public fb: FirebaseService, private toastCtrl: ToastController) {
     this.key = this.navParams.data;
     this.topics = this.fb.topics;
     this.firebaseTopics = this.fb.firebaseTopics;
@@ -48,12 +47,20 @@ export class DebatePage {
       this.topics[this.key].proClicks += 1;
       this.firebaseTopics.update(this.key , {proClicks: this.topics[this.key].proClicks});
       this.calcPerc();
-    } else {
-      Toast.show("You already voted. You can't change your vote", "5000", "center").subscribe(
-        toast => {
-          console.log(toast)
-        }
-      );
+    }
+    else if(this.users[this.key].chosen == "con"){
+      this.users[this.key].chosen = "pro";
+      this.firebaseUsers.update(this.key, {chosen: this.users[this.key].chosen});
+
+      this.topics[this.key].proClicks += 1;
+      this.firebaseTopics.update(this.key, {proClicks: this.topics[this.key].proClicks});
+
+      this.topics[this.key].conClicks -= 1;
+      this.firebaseTopics.update(this.key, {conClicks: this.topics[this.key].conClicks});
+      this.calcPerc();
+    }
+    else {
+      this.presentToast();
     }
   }
 
@@ -65,6 +72,20 @@ export class DebatePage {
       this.topics[this.key].conClicks += 1;
       this.firebaseTopics.update(this.key , {conClicks: this.topics[this.key].conClicks});
       this.calcPerc();
+    }
+    else if(this.users[this.key].chosen == "pro"){
+      this.users[this.key].chosen = "con";
+      this.firebaseUsers.update(this.key, {chosen: this.users[this.key].chosen});
+
+      this.topics[this.key].proClicks -= 1;
+      this.firebaseTopics.update(this.key, {proClicks: this.topics[this.key].proClicks});
+
+      this.topics[this.key].conClicks += 1;
+      this.firebaseTopics.update(this.key, {conClicks: this.topics[this.key].conClicks});
+      this.calcPerc();
+    }
+    else {
+      this.presentToast();
     }
   }
 
@@ -88,6 +109,20 @@ export class DebatePage {
     this.firebaseTopics.update(this.key , {conClicks: this.topics[this.key].conClicks});
     this.firebaseTopics.update(this.key , {proClicks: this.topics[this.key].proClicks});
     this.firebaseTopics.update(this.key , {totalClicks: this.topics[this.key].totalClicks});
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: "Cannot Vote More Than Once.",
+      duration: 3000,
+      position: "bottom"
+    });
+
+    toast.onDidDismiss(() => {
+      console.log("Dismissed toast");
+    });
+
+    toast.present();
   }
   // this is a comment, for update purposes
 }
