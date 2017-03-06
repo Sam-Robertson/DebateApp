f5ea8099ded530debc1409fc5f34de975c44a63e
Sam Robertson <robertsonnew@gmail.com>
2017-03-06 08:08:39 -0700

    I deleted all the image profile stuff

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,32 +2,35 @@ import { Component } from '@angular/core';
 import {NavController, NavParams, Nav} from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import {ModalController} from 'ionic-angular';
-import {LoginPage, ImagePage} from '../pages';
+import {LoginPage} from '../pages';
 import {AuthService} from '../../providers/auth-service';
-// import {Pipe, PipeTransform } from '@angular/core'
 
 
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
-  // providers: [ImagePage]
+
 })
 export class SettingsPage {
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public modalCtrl: ModalController, public nav: Nav,
-              private _auth: AuthService) { }
-   username = this._auth.displayName();
-   // initials =  I need to filter the username in order to get the initials
+              private _auth: AuthService) {
+  }
+
+  username: string = this._auth.displayName();
+
+  // initials =  I need to filter the username in order to get the initials
 
   showColorAlert() {
     let blind = this.alertCtrl.create({
       title: 'You Been Pranked!',
       subTitle: 'Here at DebateApp we believe in color blind & non-color blind people living in harmony. This is why we make all our Websites and Apps colorblind friendly to begin with.',
       buttons: [
-          {text: 'OK'},
-        {text: 'I hate you',
+        {text: 'OK'},
+        {
+          text: 'I hate you',
           handler: () => {
             this.crashSite()
           }
@@ -36,55 +39,37 @@ export class SettingsPage {
     });
     blind.present();
   }
-  logout(): void{
+
+  logout(): void {
     this._auth.signOut();
     this.nav.setRoot(LoginPage);
   }
-   private crashSite() : void {
-     let check = this.alertCtrl.create({
-       title: 'Check Yourself Before You Wreck Yourself!',
-       subTitle: 'This Website will not be kind to you from now on!',
-       buttons: [
-         {text: 'I go to my Death',
-           handler: () => {
+
+  private crashSite(): void {
+    let check = this.alertCtrl.create({
+      title: 'Check Yourself Before You Wreck Yourself!',
+      subTitle: 'This Website will not be kind to you from now on!',
+      buttons: [
+        {
+          text: 'I go to my Death',
+          handler: () => {
             this.dead()
           }
-         }
-       ]
-     });
-     check.present();
-    };
-  private dead() : void {
+        }
+      ]
+    });
+    check.present();
+  };
+
+  private dead(): void {
 
     setTimeout(function () {
       window.open("________You_Are_Now_Dead______Because_we_hate_you_____")
     }, 1500);
   };
-
-  presentProfileModal() {
-    let profileModal = this.modalCtrl.create(ImagePage);
-    profileModal.present();
-  }
-
-  
-//   Imagestuff () {
-//     let lastImage = newfilename;
-//     console.log(newfilename);
-//   }
 }
-// @Pipe ({
-//   name: 'Initials'
-// })
-// export class InitialsPipe implements PipeTransform {
-//   transform(value: string): string {
-//     let Myfirst = value.length[0];
-//     for (var char of username) {
-//       if (value.[i] === " " ){
-//         ;
-//       }
-//     }
-//   }
-// }
+
+
 
 
 
